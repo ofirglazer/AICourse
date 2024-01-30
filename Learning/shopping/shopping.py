cb91749ff27550b8eb42ee81014423aee20a8f88
Ofir Glazer <ofirglazer@gmail,com>
2024-01-31 00:01:42 +0200

    load_data completed

--- Learning/shopping/shopping.py
+++ Learning/shopping/shopping.py
@@ -59,7 +59,27 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+
+    months = {'Jan': 0, 'Feb': 1, 'Mar': 2, 'Apr': 3, 'May': 4, 'June': 5,
+              'Jul': 6, 'Aug': 7, 'Sep': 8, 'Oct': 9, 'Nov': 10, 'Dec': 11}
+
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        evidence = []
+        labels = []
+
+        for row in reader:
+            evidence.append([int(row[0]), float(row[1]),
+                             int(row[2]), float(row[3]), int(row[4]),
+                             float(row[5]), float(row[6]), float(row[7]),
+                             float(row[8]), float(row[9]), months[row[10]],
+                             int(row[11]), int(row[12]), int(row[13]), int(row[14]),
+                             (1 if row[15] == 'Returning_Visitor' else 0),
+                             (1 if row[16] == 'TRUE' else 0)])
+            labels.append(1 if row[17] == 'TRUE' else 0)
+
+        return (evidence, labels)
 
 
 def train_model(evidence, labels):
