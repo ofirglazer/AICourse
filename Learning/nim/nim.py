4bfe625d7dd1ff18b5f0860aeb0517448f571f3d
Ofir Glazer <ofirglazer@gmail,com>
2024-02-02 23:31:58 +0200

    nim complete

--- Learning/nim/nim.py
+++ Learning/nim/nim.py
@@ -101,13 +101,16 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        q_value = self.q.get((tuple(state), action))
+        if q_value is None:
+            return 0
+        return q_value
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
         Update the Q-value for the state `state` and the action `action`
         given the previous Q-value `old_q`, a current reward `reward`,
-        and an estiamte of future rewards `future_rewards`.
+        and an estimate of future rewards `future_rewards`.
 
         Use the formula:
 
@@ -118,7 +121,8 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        new_Q = old_q + self.alpha * (reward + future_rewards - old_q)
+        self.q[(tuple(state), action)] = new_Q
 
     def best_future_reward(self, state):
         """
@@ -130,7 +134,13 @@ class NimAI():
         Q-value in `self.q`. If there are no available actions in
         `state`, return 0.
         """
-        raise NotImplementedError
+        best = 0
+        possible_moves = Nim.available_actions(state)
+        for move in possible_moves:
+            value = self.get_q_value(state, move)
+            if value > best:
+                best = value
+        return best
 
     def choose_action(self, state, epsilon=True):
         """
@@ -147,7 +157,17 @@ class NimAI():
         If multiple actions have the same Q-value, any of those
         options is an acceptable return value.
         """
-        raise NotImplementedError
+        best = self.best_future_reward(state)
+        possible_moves = Nim.available_actions(state)
+        for move in possible_moves:
+            value = self.get_q_value(state, move)
+            if value == best:
+                break
+
+        if epsilon and (random.random() < self.epsilon):
+            move = random.choice(list(possible_moves))
+
+        return move
 
 
 def train(n):
